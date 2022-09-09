a7f6756b4307411aa39eb580c07d8a9b7f75fba2
vincent lavoie whitlock <1966857@cstj.qc.ca>
2022-09-09 13:47:32 -0400

    fin du ui

--- Exercice/vlwForms/MainWindow.xaml.cs
+++ Exercice/vlwForms/MainWindow.xaml.cs
@@ -18,13 +18,42 @@ namespace vlwForms
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    public enum Province
+    {
+        QC,
+        ON,
+        BC
+
+    }
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            
-            
+
+            GetControlValues();
         }
+        private void GetControlValues()
+        {
+            if (province.SelectedItem != null)
+            {
+                var selectedComboBoxItem = (ComboBoxItem)province.SelectedItem;
+                string value = selectedComboBoxItem.Content.ToString(); // Patate (string)
+
+                // Il est suggere d'utiliser des enums au lieu de string pour manipuler des choix
+                // Le bout de code ci-dessous n'est pas a retenir par coeur, il converti une string and sa valeur Enum
+                Province p = (Province)Enum.Parse(typeof(Province), value);
+            }
+
+            UpdateUI();
+        }
+        private void UpdateUI()
+        {
+      
+        }
+
+      
     }
 }
